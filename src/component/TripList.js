18163021490520c22d7f8337342ad1722bec8b79
Avishek-kr <ak251997@gmail.com>
2021-11-14 15:43:35 +0530

    handling errors

--- src/component/TripList.js
+++ src/component/TripList.js
@@ -3,14 +3,15 @@ import useFetch from '../hooks/UseFetch'
 import TripCom from './TripCom'
 
 function TripList() {
-    const [url, setUrl]= useState('http://localhost:3000/trips')
-    const { data: trips, isPending} =  useFetch(url)
+    const [url, setUrl]= useState('http://localhost:3000/tripsfgfg')
+    const { data: trips, isPending, error} =  useFetch(url)
 
 
     return (
         <div style={{width:"480px", margin: "60px auto"}}>
         <h2>Trips List</h2>
         {isPending && <div>Loading trips...</div>}
+        {error && <div>{error}</div>}
         {trips &&
         trips.map((trip)=>(
             <TripCom key={trip.id} title={trip.title} price={trip.price} />
